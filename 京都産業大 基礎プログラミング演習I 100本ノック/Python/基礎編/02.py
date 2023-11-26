93f2f5f546d4b152aeae09c019b332eb75b45470
Ak1Rα <134934470+Ak1RaGGWP@users.noreply.github.com>
2023-11-26 23:34:46 +0900

    Add files via upload

---
+++ 京都産業大 基礎プログラミング演習I 100本ノック/Python/基礎編/02.py
@@ -0,0 +1,10 @@
+
+divided = 12345 #割られる数
+divisor = 7 #割る数
+
+quotient, remain = divmod(divided, divisor)
+
+print(f"{divided} ÷ {divisor} の余りは {remain}")
+
+#参考サイト
+#https://www.freecodecamp.org/japanese/news/python-print-variable-how-to-print-a-string-and-variable/
