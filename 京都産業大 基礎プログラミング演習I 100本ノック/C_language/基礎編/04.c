431c404c498ed3a764a84fd84807f55a794b6294
Ak1Rα <134934470+Ak1RaGGWP@users.noreply.github.com>
2023-11-27 19:47:06 +0900

    Add files via upload

---
+++ 京都産業大 基礎プログラミング演習I 100本ノック/C_language/基礎編/04.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+	int num, ans;
+	
+	printf("input number: \n");
+	scanf("%d", &num);
+
+    ans = num * 3;
+
+	printf("answer = %d\n", ans); 
+	
+	return 0;
+}
